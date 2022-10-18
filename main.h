43b48aafd9002e686a88a0848376568717ca8001
Olabisi <bisiadesujo@yahoo.com>
2022-10-18 13:47:36 -0700

    add the prototypes

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <limits.h>
+
+/* list of prototypes */
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+
+#endif
